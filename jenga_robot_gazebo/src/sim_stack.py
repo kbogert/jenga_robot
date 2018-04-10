e4a669e0ec6ac4a6b5172d445f4010d744529db5
Kenneth Bogert <kbogert@unca.edu>
2018-04-09 22:51:17 -0400

    Got jenga system working
    Need to export the move block interface as a service

--- jenga_robot_gazebo/src/sim_stack.py
+++ jenga_robot_gazebo/src/sim_stack.py
@@ -4,23 +4,29 @@
 import roslib; roslib.load_manifest('jenga_robot_gazebo')
 import rospy
 from gazebo_msgs.srv import DeleteModel, SpawnModel, GetModelState
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, Int32
 from geometry_msgs.msg import *
 
 block_urdf = None
-base_pose = None
+block_pose = None
+spawn_srv = None
+delete_srv = None
+model_states = None
+
+blocks_list = []
 
 # need the store the state of all blocks, for building the state message
 # need to provide an actionlib interface for removing a block and placing it on top
 
 stack_state = []
+block_pos_to_id = {}
 
 
-def spawn_new_block(spawn_srv, new_id, block_pose):
+def spawn_new_block(new_id, pose):
 
-	spawn_srv(new_id, block_urdf, "", block_pose, "world")
+	spawn_srv(new_id, block_urdf, "", pose, "world")
 
-def spawn_all_blocks(spawn_srv, blocks_list, num_levels):
+def spawn_all_blocks(num_levels):
 
 	rotated = False
 	count = 0
@@ -58,14 +64,16 @@ def spawn_all_blocks(spawn_srv, blocks_list, num_levels):
 
 		for j in range(3):
 			newid = "block" + str(count)
-			count += 1
-			spawn_new_block(spawn_srv, newid, next_pose)
+			spawn_new_block(newid, next_pose)
 			if rotated:
 				next_pose.position.x += 0.0255 # BLOCK WIDTH is .025
 			else:
 				next_pose.position.y += 0.0255 # BLOCK WIDTH is .025
 			blocks_list.append(newid)
 			stack_state[i].append(1)
+			block_pos_to_id[count] = newid
+
+			count += 1
 
 		next_pose.position.z += 0.015
 		tmp_rotation = next_pose.orientation
@@ -74,35 +82,55 @@ def spawn_all_blocks(spawn_srv, blocks_list, num_levels):
 
 		rotated = not rotated
 
-def delete_all_blocks(delete_srv, blocks_list):
+	print(block_pos_to_id)
+
+def delete_all_blocks():
 
 	for block in blocks_list:
 		delete_srv(block)
 
+	blocks_list[:] = []
+	block_pos_to_id = {}
 
-def hasFallen(model_states, blocks_list):
+def hasFallen():
 
 
 	if len(blocks_list) < 3:
 		return False
 
-	# average the z-height of each block's position, if below half of the ideal block height, we've fallen
-
-	block_height = 0.015
-	total_height = 0
+	# check if there are any blocks outside of the stack's dimensions
 
 	for block in blocks_list:
-		total_height += model_states(block, "world").pose.position.z - block_pose.position.z
+		pos =  model_states(block, "world").pose.position
+
+		if (pos.x < block_pose.position.x - (0.025 * 2.5)) or ( pos.x > block_pose.position.x + (0.025 * 4.5)) or ( pos.y < block_pose.position.y - (0.025 * 1)) or (pos.y > block_pose.position.y + (0.025 * 4.5) ):
+
+			print("Fall detected")
+			print(block)
+			print(pos)
+			return True
+
+	return False
+
+	# average the z-height of each block's position, if below half of the ideal block height, we've fallen
+
+#	block_height = 0.015
+#	total_height = 0
+
+#	for block in blocks_list:
+#		total_height += model_states(block, "world").pose.position.z - block_pose.position.z
 	
 #	total_height /= 3
 
-	avg_height = total_height / (len(blocks_list) / 3)
-
-	if avg_height < (len(blocks_list) / 3) /2 * block_height:
-		# we've fallen
-		return True
-
-	return False
+#	avg_height = total_height / (len(blocks_list) / 3)
+#
+#	print(str(avg_height) + " " + str((len(blocks_list) / 3) /2 * block_height)) 
+#
+#	if avg_height < (len(blocks_list) / 3) /2 * block_height:
+#		# we've fallen
+#		return True
+#
+#	return False
 
 def getBlockPose(model_states, id):
 
@@ -124,6 +152,90 @@ def publishState(state_pub):
 	state_pub.publish(toPub)
 
 
+def moveBlock(req):
+
+	blocknum = req.data
+
+	blockCount = 0
+	blockpos = 0
+
+	for level in stack_state:
+
+		if blockCount > blocknum:
+			break
+
+		for entry in level:
+			if entry == 1:
+				blockCount += 1
+
+			if blockCount > blocknum:
+				break
+
+			blockpos += 1	
+			
+	blockid = str(block_pos_to_id[blockpos])
+
+	delete_srv(blockid)
+	stack_state[int(blockpos / 3)][blockpos % 3] = 0
+
+	# now the block has been removed, figure out where to place it	
+
+
+	allFull = True
+
+	for entry in stack_state[len(stack_state) - 1]:
+		if entry == 0:
+			allFull = False
+			break
+
+	if allFull:
+		
+		# the upper level is full, add on to our stack state in prep for 
+		stack_state.append([0,0,0])
+		
+
+	whichRotation = len(stack_state) % 2 == 0
+	z_height = block_pose.position.z + (len(stack_state) - 1) * 0.015
+
+	pos = 0
+
+	for entry in stack_state[len(stack_state) - 1]:
+		if (entry == 1):
+			pos += 1
+		else:
+			break
+
+        next_pose = Pose()
+    
+        next_pose.position.x = block_pose.position.x
+        next_pose.position.y = block_pose.position.y
+        next_pose.position.z = z_height
+
+	if not whichRotation:
+	        next_pose.orientation.x = block_pose.orientation.x
+	        next_pose.orientation.y = block_pose.orientation.y
+	        next_pose.orientation.z = block_pose.orientation.z
+	        next_pose.orientation.w = block_pose.orientation.w
+
+		next_pose.position.y += 0.0255 * pos
+	else:
+        	next_pose.orientation.x = 0.499
+        	next_pose.orientation.y = -0.499
+        	next_pose.orientation.z = 0.501
+        	next_pose.orientation.w = 0.501
+
+		next_pose.position.y = block_pose.position.y + 0.025
+		next_pose.position.x = block_pose.position.x - 0.025
+	
+		next_pose.position.x += 0.0255 * pos
+		
+	spawn_new_block(blockid, next_pose)
+
+	del(block_pos_to_id[blockpos])
+
+	block_pos_to_id[pos + (len(stack_state)-1) * 3] = blockid
+	stack_state[len(stack_state) - 1][pos] = 1
+
 if __name__=='__main__':
 
 	rospy.init_node('jenga_block_manager')
@@ -137,8 +249,12 @@ if __name__=='__main__':
 	gazebo_spawn_model_srv = rospy.ServiceProxy("gazebo/spawn_urdf_model", SpawnModel)
 	gazebo_delete_model_srv = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
 
+	spawn_srv = gazebo_spawn_model_srv
+	delete_srv = gazebo_delete_model_srv
+
 	gazebo_model_states_srv = rospy.ServiceProxy("gazebo/get_model_state", GetModelState)
-	
+	model_states = gazebo_model_states_srv
+
 	state_pub = rospy.Publisher('/game_msgs/state', std_msgs.msg.String, queue_size=10)
 	system_reset_pub = rospy.Publisher('/game_msgs/reset', Empty, queue_size=10)
 
@@ -162,16 +278,16 @@ if __name__=='__main__':
 
 	block_urdf = rospy.get_param('~block_description')
 
-	blocks_list = []
 	stack_constructed = False
 
 	sleep_time = 1.0 / desired_frequency
 	while not rospy.is_shutdown():
+		rospy.sleep(sleep_time)
+
 		# Do we need to destroy all blocks?
 
-		if hasFallen(gazebo_model_states_srv, blocks_list):
-			delete_all_blocks(gazebo_delete_model_srv, blocks_list)
-			blocks_list[:] = []
+		if hasFallen():
+			delete_all_blocks()
 
 			system_reset_pub.publish(Empty())
 			rospy.sleep(sleep_time)
@@ -180,12 +296,20 @@ if __name__=='__main__':
 			stack_state = []
 
 		# Do we need to add another block?
-		if not stack_constructed:
+		elif not stack_constructed:
 			
-			spawn_all_blocks(gazebo_spawn_model_srv, blocks_list, stack_height)
+			spawn_all_blocks( stack_height )
 			stack_constructed = True
 			publishState(state_pub)
 
+		else:
 
-		rospy.sleep(sleep_time)
+			import random
+
+			msg = Int32()
+			msg.data = random.randint(0, len(blocks_list) - 1)
+		
+			print("Moving block " + str(msg.data))
+
+			moveBlock(msg)
 
